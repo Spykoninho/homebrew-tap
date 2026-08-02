fc6972ff8f75e31dca03ca21a435646c0fba4f76
Mathis Fremiot <mathis.fremiot@enedis.fr>
2026-08-02 17:45:41 +0200

    fix: sha256 du DMG v1.1 publié

--- Casks/slotch.rb
+++ Casks/slotch.rb
@@ -5,7 +5,7 @@
 #   brew install --cask spykoninho/tap/slotch
 cask "slotch" do
   version "1.1"
-  sha256 "05bff3a69b9cb84faf8d5eff91998b7331a1451a71687be79b59f462a89f0aa5"
+  sha256 "4ddd80f6b79c26ad86ac53ce327ea3f03efa16e9daadf090c19829f0d3f10fbd"
 
   url "https://github.com/Spykoninho/SlotNotch/releases/download/#{version}/Slotch-#{version}.dmg"
   name "Slotch"
