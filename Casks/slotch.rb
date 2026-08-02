28ad090889db5c41219709fd6fa1fac2db585303
Mathis Fremiot <mathis.fremiot@enedis.fr>
2026-08-02 21:34:06 +0200

    chore: URL du repo renommé Slotch

--- Casks/slotch.rb
+++ Casks/slotch.rb
@@ -7,10 +7,10 @@ cask "slotch" do
   version "1.2"
   sha256 "6a2fe3f77e5fed69a8079bfe12451836ad20ab1152bd4f8f9a31387e339bbb55"
 
-  url "https://github.com/Spykoninho/SlotNotch/releases/download/#{version}/Slotch-#{version}.dmg"
+  url "https://github.com/Spykoninho/Slotch/releases/download/#{version}/Slotch-#{version}.dmg"
   name "Slotch"
   desc "Slot machine hiding behind your Mac's notch"
-  homepage "https://github.com/Spykoninho/SlotNotch"
+  homepage "https://github.com/Spykoninho/Slotch"
 
   depends_on macos: :monterey
 
